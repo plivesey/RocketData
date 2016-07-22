1bc360380702de725b68bff32867af661a80c8af
Peter Livesey <plivesey@linkedin.com>
2016-07-22 10:34:54 -0700

    Adding ChatsViewController Switching from storyboards to nibs

---
+++ SampleApp/SampleApp/ChatsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ChatsViewController.swift
+//  SampleApp
+//
+//  Created by Peter Livesey on 7/22/16.
+//  Copyright © 2016 LinkedIn. All rights reserved.
+//
+
+import UIKit
+
+class ChatsViewController: UIViewController {
+
+    init() {
+        super.init(nibName: "ChatsViewController", bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
