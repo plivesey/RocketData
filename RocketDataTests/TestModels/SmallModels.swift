10f984fd21670398318bc97ff5d3f44e6e12d5ad
Peter Livesey <plivesey@linkedin.com>
2016-08-16 15:43:19 -0700

    Added a test for this fix

---
+++ RocketDataTests/TestModels/SmallModels.swift
@@ -0,0 +1,45 @@
+// © 2016 LinkedIn Corp. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+import Foundation
+import RocketData
+
+/*
+ These classes aren't actually used in tests.
+ They are here to test that they compile. They are the smallest versions of Model and SimpleModel and neither are final.
+ If they compile, the test passes.
+ */
+
+class SmallSimpleModel: SimpleModel {
+    var modelIdentifier: String? {
+        return nil
+    }
+
+    func isEqualToModel(model: SimpleModel) -> Bool {
+        return true
+    }
+}
+
+class SmallModel: Model {
+    var modelIdentifier: String? {
+        return nil
+    }
+
+    func isEqualToModel(model: Model) -> Bool {
+        return true
+    }
+
+    func map(transform: Model -> Model?) -> Self? {
+        return self
+    }
+
+    func forEach(visit: Model -> Void) {
+    }
+}
+
