af2cd392deb88c154809a54c5a51c36fba880751
Peter Livesey <plivesey@linkedin.com>
2016-09-22 08:28:21 -0700

    Changing the SDK and adding tests for iOS 10 Likely it will fail because the iOS 8 simulators are flaky, but going to try it

--- build.sh
+++ build.sh
@@ -4,8 +4,9 @@ set -o pipefail &&
 time xcodebuild clean test \
     -workspace RocketData.xcworkspace \
     -scheme RocketData \
-    -sdk iphonesimulator9.3 \
+    -sdk iphonesimulator10.0 \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=8.4' \
+    -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.0' \ 
 | xcpretty
 
