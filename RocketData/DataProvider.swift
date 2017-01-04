3218e9a184f8970e46954fc037dee2c428725e40
Peter Livesey <plivesey@linkedin.com>
2017-01-04 11:24:33 -0800

    Adding comments and cleaning up some code

--- RocketData/DataProvider.swift
+++ RocketData/DataProvider.swift
@@ -53,7 +53,15 @@ open class DataProvider<T: SimpleModel>: ConsistencyManagerListener, BatchListen
             }
         }
     }
-    
+
+    /**
+     This property returns the model identifier which is associated with this `DataProvider`.
+     It either returns the id of the data currently set, or an id that it's listening to (if the model doesn't exist yet).
+     If you don't have a model yet, but want to listen to a model which may exist in the future, you can set
+     this property to an ID.
+     You should NOT do this if you have already set data (this will throw an assertion).
+     After setting this property, if the model is updated elsewhere, the delegate will be called and the new model will be set.
+     */
     open var modelIdentifier: String? {
         get {
             if let data = data {
@@ -69,12 +77,11 @@ open class DataProvider<T: SimpleModel>: ConsistencyManagerListener, BatchListen
             }
             listeningToModelIdentifier = newValue
             if newValue != nil {
+                // If we start listening to a new ID, we need to add ourselves as a global listener
                 dataModelManager.consistencyManager.addModelUpdatesListener(self)
             }
         }
     }
-    
-    var listeningToModelIdentifier: String?
 
     /// This saves the batchListener instance. It is public because it implements the BatchListenable protocol. You should never edit this directly.
     open weak var batchListener: BatchDataProviderListener?
@@ -85,11 +92,18 @@ open class DataProvider<T: SimpleModel>: ConsistencyManagerListener, BatchListen
     var dataHolder = DataHolder<T?>(data: nil) {
         didSet {
             if dataHolder.data != nil && listeningToModelIdentifier != nil {
+                // If we set new data, let's erase the ID we're listening to and stop listening to updates
                 listeningToModelIdentifier = nil
                 dataModelManager.consistencyManager.removeModelUpdatesListener(self)
             }
         }
     }
+
+    /**
+     This is the internal storage for the ID we are listening to.
+     If `data` is not nil, this should be nil.
+     */
+    var listeningToModelIdentifier: String?
 
     /// This is updated whenever we set data. In some circumstances, we want to check that our new update is newer than our current model.
     var lastUpdated: ChangeTime {
@@ -136,6 +150,8 @@ open class DataProvider<T: SimpleModel>: ConsistencyManagerListener, BatchListen
      - parameter completion: Called on the main thread. This is called with the result from the cache.
      At this point, the data provider will already have new data, so there's no need to call setData.
      This completion block will always be called exactly once, even if no data was updated.
+
+     TODO: Once we do a major version bump, remove this method and use the other method with an optional parameter.
      */
     open func fetchDataFromCache(withCacheKey cacheKey: String?, context: Any? = nil, completion: @escaping (T?, NSError?)->()) {
 
@@ -172,15 +188,18 @@ open class DataProvider<T: SimpleModel>: ConsistencyManagerListener, BatchListen
      This is because if we have data, it should be identical to the cached data so fetching from the cache is pointless.
      
      - parameter cacheKey: The cache key for this model.
+     - parameter listenToModelIdentifier: If true, then the data provider will start listening to this cache key
+     even if the cache misses. This means if the cache misses, but then the model is added to the cache later,
+     the data provider will get that new data.
      - parameter context: This context is passed to the cacheDelegate when making the query. Default nil.
      - parameter completion: Called on the main thread. This is called with the result from the cache.
      At this point, the data provider will already have new data, so there's no need to call setData.
      This completion block will always be called exactly once, even if no data was updated.
      */
     open func fetchDataFromCache(withCacheKey cacheKey: String?, listenToModelIdentifier: Bool, context: Any? = nil, completion: @escaping (T?, NSError?)->()) {
-        
         fetchDataFromCache(withCacheKey: cacheKey, context: context) { model, error in
             if self.data == nil && model == nil && listenToModelIdentifier {
+                // Only if our current data is nil and the cache missed should we start listening to this ID
                 self.modelIdentifier = cacheKey
             }
             completion(model, error)
@@ -200,19 +219,18 @@ open class DataProvider<T: SimpleModel>: ConsistencyManagerListener, BatchListen
         }
     }
 
-    // MARK: Consistency Manager Implementation
-
-    open func currentModel() -> ConsistencyManagerModel? {
-        return data
-    }
-
-    open func modelUpdated(_ model: ConsistencyManagerModel?, updates: ModelUpdates, context: Any?) {
+    /**
+     Given a context, it assumes it is a ConsistencyContextWrapper.
+     It then unwraps a change time and actual context form this object with defaults.
+     It uses nil as a sentinal value to signify that this operation should be discarded (the change time is out of date).
+     */
+    func changeTimeAndContext(fromContext context: Any?) -> (changeTime: ChangeTime, actualContext: Any?)? {
         let actualContext: Any?
-        var changeTime: ChangeTime?
+        let changeTime: ChangeTime
         if let context = context as? ConsistencyContextWrapper {
             if !context.creationDate.after(lastUpdated) {
                 // Our current data is newer than this change so let's discard this change.
-                return
+                return nil
             }
             actualContext = context.context
             changeTime = context.creationDate
@@ -220,6 +238,22 @@ open class DataProvider<T: SimpleModel>: ConsistencyManagerListener, BatchListen
             // The change came from a manual change to the consistency manager so we don't have time information
             // This isn't preferable, but let's assume that we actually want this change
             actualContext = context
+            changeTime = ChangeTime()
+        }
+
+        return (changeTime, actualContext)
+    }
+
+    // MARK: Consistency Manager Implementation
+
+    open func currentModel() -> ConsistencyManagerModel? {
+        return data
+    }
+
+    open func modelUpdated(_ model: ConsistencyManagerModel?, updates: ModelUpdates, context: Any?) {
+        guard let (changeTime, actualContext) = changeTimeAndContext(fromContext: context) else {
+            // This signifies that this change should be discarded because it's out of date
+            return
         }
 
         // Here, we are casting to T? so we catch nil. If the model is nil, it means it was deleted, so we should set data to nil
@@ -227,7 +261,7 @@ open class DataProvider<T: SimpleModel>: ConsistencyManagerListener, BatchListen
             // It will already have been updated in the cache so we don't need to recache it
             // We are also already listening to the new model so don't need to call listenForUpdates again
             // If we updated ourselves through Rocket Data, we'll always have a ChangeTime. Otherwise, let's use now.
-            dataHolder.setData(model, changeTime: changeTime ?? ChangeTime())
+            dataHolder.setData(model, changeTime: changeTime)
             delegate?.dataProviderHasUpdatedData(self, context: actualContext)
         } else {
             Log.sharedInstance.assert(false, "Consistency manager returned an incorrect model type. It looks like we have duplicate ids for different classes. This is not allowed because models must have globally unique identifiers.")
@@ -238,27 +272,20 @@ open class DataProvider<T: SimpleModel>: ConsistencyManagerListener, BatchListen
                             updatedModel model: ConsistencyManagerModel,
                             changes: [String: ModelChange],
                             context: Any?) {
+        // We only care about changes if we have a modelId and data is nil
         if let modelId = listeningToModelIdentifier, data == nil {
+            // Let's see if we care about any of these changes
             if let modelChange = changes[modelId] {
+                // We only care if the model is updated. If it's deleted, we ignore it and still keep listening
                 if case ModelChange.updated(let models) = modelChange {
                     models.forEach { newModel in
                         // There may be multiple projections here. Let's search for one of the right class.
                         if let newModel = newModel as? T {
-                            let actualContext: Any?
-                            var changeTime: ChangeTime?
-                            if let context = context as? ConsistencyContextWrapper {
-                                if !context.creationDate.after(lastUpdated) {
-                                    // Our current data is newer than this change so let's discard this change.
-                                    return
-                                }
-                                actualContext = context.context
-                                changeTime = context.creationDate
-                            } else {
-                                // The change came from a manual change to the consistency manager so we don't have time information
-                                // This isn't preferable, but let's assume that we actually want this change
-                                actualContext = context
+                            guard let (changeTime, actualContext) = changeTimeAndContext(fromContext: context) else {
+                                // This signifies that this change should be discarded because it's out of date
+                                return
                             }
-                            dataHolder.setData(newModel, changeTime: changeTime ?? ChangeTime())
+                            dataHolder.setData(newModel, changeTime: changeTime)
                             delegate?.dataProviderHasUpdatedData(self, context: actualContext)
                         }
                     }
@@ -266,6 +293,7 @@ open class DataProvider<T: SimpleModel>: ConsistencyManagerListener, BatchListen
                 
             }
         } else {
+            // Otherwise, let's stop listening because we don't care anymore
             dataModelManager.consistencyManager.removeModelUpdatesListener(self)
         }
     }
