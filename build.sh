e7dc93040e8a355688044cd8fbb5ec9406e84411
Kashish <gkashish98@gmail.com>
2018-01-26 12:06:33 -0500

    travis simulator destination fix

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ time xcodebuild clean test \
     -scheme RocketData \
     -sdk iphonesimulator11.2 \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' \
-    -destination 'platform=iOS Simulator,name=iPhone 7,OS=11.0' \
+    -destination 'platform=iOS Simulator,name=iPhone X,OS=11.1' \
 | xcpretty
 
 # Disabling 8.4 because it's very flaky on travis
