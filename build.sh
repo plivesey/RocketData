eaf8fb383ba7d847901940df6ae2cb0fe858e22c
Kashish Goel <gkashish98@gmail.com>
2018-02-12 18:36:48 -0500

    Swift 4 conversion (#76)
    * Ran xcode swift 4 converter
    
    * Update travis osx_image to xcode9.2
    
    * updated build.sh simulator package for swift 4 migration
    
    * update iphone simulator sdk to 11.2 in build.sh
    
    * travis simulator destination fix

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ set -o pipefail &&
 time xcodebuild clean test \
     -workspace RocketData.xcworkspace \
     -scheme RocketData \
-    -sdk iphonesimulator10.0 \
+    -sdk iphonesimulator11.2 \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' \
-    -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.0' \
+    -destination 'platform=iOS Simulator,name=iPhone X,OS=11.1' \
 | xcpretty
 
 # Disabling 8.4 because it's very flaky on travis
