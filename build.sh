bb56860a7927ba5a5471b084ee6bbc20814f7304
Peter Livesey <plivesey453@gmail.com>
2018-05-17 14:03:24 -0700

    Updating the xcode version and build script for travis

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o pipefail &&
 time xcodebuild clean test \
     -workspace RocketData.xcworkspace \
     -scheme RocketData \
-    -sdk iphonesimulator11.2 \
+    -sdk iphonesimulator \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' \
     -destination 'platform=iOS Simulator,name=iPhone X,OS=11.1' \
 | xcpretty
