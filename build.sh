922a27ceb788a5d937819dfd3f963317d1e4dad5
Peter Livesey <plivesey453@gmail.com>
2018-09-21 12:43:00 -0700

    Adding a comment for travis fix

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ time xcodebuild clean test \
     -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.1' \
     -destination 'platform=iOS Simulator,name=iPhone X,OS=11.3' \
     -destination 'platform=iOS Simulator,name=iPhone XS Max,OS=12.0' \
+    # This appears to fix travis on Xcode10 - https://stackoverflow.com/questions/52395946/xcodebuild-software-caused-connection-abort-userinfo-nslocalizeddescription-e
     -disable-concurrent-destination-testing \
 | xcpretty
 
