373e191d5c7c274830660925b0d3ec34c5ee4efe
Peter Livesey <plivesey453@gmail.com>
2018-12-04 11:51:05 +0700

    Attempting to move around disable-concurrent-destination-testing to get travis to work

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
+# disable-concurrent-destination-testing appears to fix travis on Xcode10 - https://stackoverflow.com/questions/52395946/xcodebuild-software-caused-connection-abort-userinfo-nslocalizeddescription-e
+
 set -o pipefail &&
 time xcodebuild clean test \
     -workspace RocketData.xcworkspace \
     -scheme RocketData \
     -sdk iphonesimulator \
+    -disable-concurrent-destination-testing \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' \
     -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.1' \
     -destination 'platform=iOS Simulator,name=iPhone X,OS=11.3' \
     -destination 'platform=iOS Simulator,name=iPhone XS Max,OS=12.0' \
-    # This appears to fix travis on Xcode10 - https://stackoverflow.com/questions/52395946/xcodebuild-software-caused-connection-abort-userinfo-nslocalizeddescription-e
-    -disable-concurrent-destination-testing \
 | xcpretty
 
 # Disabling 8.4 because it's very flaky on travis
