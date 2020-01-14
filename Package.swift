846450cc1ca8a008f00959db032cce4dc89826ba
Heberti Almeida <hebertialmeida@gmail.com>
2020-01-14 09:50:40 -0500

    Added Swift Package files

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "RocketData",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v8),
+        .tvOS(.v9),
+        .watchOS(.v3)
+    ],
+    products: [
+        .library(name: "RocketData", targets: ["RocketData"]),
+    ],
+    dependencies: [
+        .package(path: "../ConsistencyManager-iOS")
+    ],
+    targets: [
+        .target(
+            name: "RocketData", 
+            dependencies: ["ConsistencyManager"],
+            path: "RocketData"
+        ),
+        .testTarget(
+            name: "RocketDataTests",
+            dependencies: ["RocketData", "ConsistencyManager"],
+            path: "RocketDataTests"
+        )
+    ],
+    swiftLanguageVersions: [.v4, .v4_2, .v5]
+)
