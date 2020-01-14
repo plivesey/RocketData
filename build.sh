71881225b115f4f4218eb71f31a4936936a00130
Heberti Almeida <hebertialmeida@gmail.com>
2020-01-14 11:42:31 -0500

    Add Swift Package Manager support (#94)
    * Removed Cocoapods files
    
    * Removing unnecessary files due to SPM
    
    * Fixed compiler warnings
    
    * Added Swift Package files
    
    * Updating build script and travis
    
    * Fix package reference to point to github's version

--- build.sh
+++ build.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 
-# disable-concurrent-destination-testing appears to fix travis on Xcode10 - https://stackoverflow.com/questions/52395946/xcodebuild-software-caused-connection-abort-userinfo-nslocalizeddescription-e
-
 set -o pipefail &&
 time xcodebuild clean test \
-    -workspace RocketData.xcworkspace \
+    -scheme RocketData \
+    -sdk macosx \
+    -enableCodeCoverage YES \
+    | tee build.log \
+    | xcpretty &&
+time xcodebuild clean test \
     -scheme RocketData \
     -sdk iphonesimulator \
     -disable-concurrent-destination-testing \
-    -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' \
-    -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.1' \
-    -destination 'platform=iOS Simulator,name=iPhone X,OS=11.3' \
-    -destination 'platform=iOS Simulator,name=iPhone XS Max,OS=12.2' \
-| xcpretty
-
-# Disabling 8.4 because it's very flaky on travis
-# We can look at reenabling when it gets more stable
-#    -destination 'platform=iOS Simulator,name=iPhone 6,OS=8.4' \
-
+    -enableCodeCoverage YES \
+    -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.3.1' \
+    -destination 'platform=iOS Simulator,name=iPhone 7,OS=11.4' \
+    -destination 'platform=iOS Simulator,name=iPhone X,OS=12.4' \
+    -destination 'platform=iOS Simulator,name=iPhone 11 Pro,OS=13.3' \
+    | tee build.log \
+    | xcpretty &&
+time xcodebuild clean test \
+    -scheme RocketData \
+    -sdk appletvsimulator \
+    -enableCodeCoverage YES \
+    -destination 'platform=tvOS Simulator,name=Apple TV,OS=13.3' \
+    | tee build.log \
+    | xcpretty &&
+cat build.log
